f9eaed10f6817b57a098dd1c401a4fa8d857e995
Aimin <huangepn@gmail.com>
2017-01-20 10:02:50 +0800

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,15 @@
+@echo off
+set "MSBuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+set cmake="F:\Program Files (x86)\CMake\bin\cmake.exe"
+
+:: rmdir /S build
+:: mkdir build
+if not exist build mkdir build
+cd build
+%cmake% ..
+
+%MSBuild% ALL_BUILD.vcxproj /nologo /t:build /p:Configuration=Debug
+%MSBuild% ALL_BUILD.vcxproj /nologo /t:build /p:Configuration=Release
+
+cd ..
+::pause
